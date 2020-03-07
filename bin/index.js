cebfa9c6c3c9f03b51bc379ac4aa576605e284bb
buraktekin <tknbrk@gmail.com>
2020-03-08 00:35:45 +0100

    ➗ separate ui options from main script

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,9 @@ const axios = require('axios')
 const yargs = require('yargs')
 const chalk = require("chalk")
 const boxen = require("boxen")
+const { boxenOptions } = require("./ui")
 const { generateURL } = require("./arcGis")
+
 
 // add args to let users be able to select a specific location
 const options = yargs
@@ -16,55 +18,62 @@ const options = yargs
     demandOption: false
   })
   .argv
+// ##############################################
 
-// Boxen for a better UI
-const boxenOptions = {
-  confirmed: {
-    padding: 1,
-    borderStyle: "round",
-    borderColor: "red",
-    backgroundColor: "red"
-  },
-  recovered: {
-    padding: 1,
-    borderStyle: "round",
-    borderColor: "green",
-    backgroundColor: "green"
-  }
-}
 
 // message to display selected location
 const message = 
   options.location ? 
-    `Selected Location: ${options.location}`
+    `Selected Location: ${options.location.toUpperCase()}`
     : 
     "No Location Selected"
-const msgBox = boxen( message, boxenOptions );
-console.log(msgBox);
+const msgBox = boxen( message, boxenOptions.locInfo )
+console.log(chalk.black.bold(msgBox))
+// ##############################################
 
 
-const url = generateURL(options.location)
-axios.get(url, { headers: { Accept: "application/json" } })
-.then(res => {
-  if (options.location) {
-    // if searching for jokes, loop over the results
-    const data = res.data.features
-    for(let i = 0; i < data.length; i++) {
-      if(data[i].attributes.Country_Region.toLowerCase() === options.location.toLowerCase()) {
-        const {Confirmed, Deaths, Recovered} = data[i].attributes
-        console.log(
-          Confirmed,
-          Deaths,
-          Recovered
-        )
-        break
-      }
-    }
-    if (data.length === 0) {
-      console.log("no data found!");
-    }
+// Display the results
+const displayResults = (data) => {
+  if( options.location ) {
+    const { Confirmed = 0, Deaths = 0, Recovered = 0 } = data
+    const msgBoxDeaths = boxen( `Deaths:\n${Deaths}`, boxenOptions.deaths )
+    const msgBoxConfirmed = boxen( `Confirmed:\n${Confirmed}`, boxenOptions.confirmed )
+    const msgBoxRecovered = boxen( `Recovered:\n${Recovered}`, boxenOptions.recovered )
+    console.log(chalk.white.bold( msgBoxDeaths ))
+    console.log(chalk.white.bold( msgBoxConfirmed ))
+    console.log(chalk.white.bold( msgBoxRecovered ))
   } else {
-    console.log(res.data.features[0]);
+    console.log(data)
   }
-});
+}
+// ##############################################
 
+
+// Fetch data and display results
+async function fetchData() {
+  const url = generateURL(options.location)
+  await axios.get(url, { headers: { Accept: "application/json" } })
+  .then(res => {
+    if (options.location) {
+      // if searching for jokes, loop over the results
+      const data = res.data.features
+      for(let i = 0; i < data.length; i++) {
+        if(data[i].attributes.Country_Region.toLowerCase() === options.location.toLowerCase()) {
+          return data[i].attributes
+        }
+      }
+      if (data.length === 0) {
+        console.log("no data found!")
+      }
+    } else {
+      return res.data.features[0].attributes
+    }
+  })
+  .then(res => {
+    displayResults(res)
+  })
+}
+// ##############################################
+
+
+fetchData()
---
+++ bin/ui.js
@@ -0,0 +1,34 @@
+// Boxen for a better UI
+const ui = {
+  boxenOptions = {
+    locInfo: {
+      padding: 1,
+      borderStyle: "bold",
+      borderColor: "yellow",
+      backgroundColor: "white"
+    },
+    confirmed: {
+      padding: 1,
+      borderStyle: "bold",
+      borderColor: "yellow",
+      backgroundColor: "yellow",
+      float: "left"
+    },
+    deaths: {
+      padding: 1,
+      borderStyle: "bold",
+      borderColor: "red",
+      backgroundColor: "red",
+      float: "left"
+    },
+    recovered: {
+      padding: 1,
+      borderStyle: "bold",
+      borderColor: "green",
+      backgroundColor: "green",
+      float: "left"
+    }
+  }
+}
+
+module.exports.ui = ui
