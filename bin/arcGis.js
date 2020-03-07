30408cee5ff3b95b93f7d9551ea9499572f6ffb1
buraktekin <tknbrk@gmail.com>
2020-03-07 23:38:14 +0100

    🤜💥🤛 updated url resources and params for location search

--- bin/arcGis.js
+++ bin/arcGis.js
@@ -1,9 +1,9 @@
 const qs = require("querystring")
 
-// extracted source from  John Hopkins CSSE dashboard :/
+// extracted source from  John Hopkins CSSE dashboard 😔
 // https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6
-const ROOT = "https://services1.arcgis.com/"
-const RESOURCES = "0MSEUqKaxRlEPj5g/arcgis/rest/services/cases_time_v3/FeatureServer/0/"
+let ROOT = "https://services1.arcgis.com/"
+let RESOURCES = "0MSEUqKaxRlEPj5g/arcgis/rest/services/cases_time_v3/FeatureServer/0/"
 
 const params = {
   f: 'json',
@@ -11,12 +11,20 @@ const params = {
   returnGeometry: 'false',
   spatialRel: 'esriSpatialRelIntersects',
   outFields: '*',
-  orderByFields: 'Report_Date_String asc',
+  orderByFields: 'Report_Date_String desc',
   resultOffset: '0',
   cacheHint: 'true'
 }
 
-const stringifyParams = () => qs.stringify(params)
-const url = `${ROOT}${RESOURCES}query?${stringifyParams()}`
+const generateURL = (location) => {
+  if(location) {
+    RESOURCES = "0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/"
+    params.where = "Confirmed > 0"
+    params.orderByFields = "Confirmed desc"
+  }
+  
+  const url = `${ROOT}${RESOURCES}query?${qs.stringify(params)}`
+  return url
+}
 
-module.exports.url = url
+module.exports.generateURL = generateURL
--- bin/index.js
+++ bin/index.js
@@ -4,8 +4,9 @@ const axios = require('axios')
 const yargs = require('yargs')
 const chalk = require("chalk")
 const boxen = require("boxen")
-const { url } = require("./arcGis")
+const { generateURL } = require("./arcGis")
 
+// add args to let users be able to select a specific location
 const options = yargs
   .usage("Usage: -l <location>")
   .option("l", {
@@ -16,17 +17,54 @@ const options = yargs
   })
   .argv
 
-const message = options.location ? 
-  `Selected Location: ${options.location}`: "No Location Selected"
-console.log(message)
-
+// Boxen for a better UI
 const boxenOptions = {
-  padding: 1,
-  borderStyle: "round",
-  borderColor: "green",
-  backgroundColor: "green"
+  confirmed: {
+    padding: 1,
+    borderStyle: "round",
+    borderColor: "red",
+    backgroundColor: "red"
+  },
+  recovered: {
+    padding: 1,
+    borderStyle: "round",
+    borderColor: "green",
+    backgroundColor: "green"
+  }
 }
 
+// message to display selected location
+const message = 
+  options.location ? 
+    `Selected Location: ${options.location}`
+    : 
+    "No Location Selected"
 const msgBox = boxen( message, boxenOptions );
 console.log(msgBox);
+
+
+const url = generateURL(options.location)
+axios.get(url, { headers: { Accept: "application/json" } })
+.then(res => {
+  if (options.location) {
+    // if searching for jokes, loop over the results
+    const data = res.data.features
+    for(let i = 0; i < data.length; i++) {
+      if(data[i].attributes.Country_Region.toLowerCase() === options.location.toLowerCase()) {
+        const {Confirmed, Deaths, Recovered} = data[i].attributes
+        console.log(
+          Confirmed,
+          Deaths,
+          Recovered
+        )
+        break
+      }
+    }
+    if (data.length === 0) {
+      console.log("no data found!");
+    }
+  } else {
+    console.log(data[0]);
+  }
+});
 
