7da79892a2920ef75909b621bc8e463be19e3187
Burak Tekin <tknbrk@gmail.com>
2020-10-19 14:01:18 +0200

    fix box for 'data not found'

--- bin/index.js
+++ bin/index.js
@@ -11,7 +11,7 @@ const { generateURL } = require("./arcGis")
 // get options to format interface
 const { boxenOptions, cfontsOptions } = ui
 
-// Render results
+// Methods
 const render = (data) => {
   return console.log(chalk.white.bold(data))
 }
@@ -20,60 +20,7 @@ const spaceFix = (msg, info) => {
   return info + ' '.repeat(msg.length - info.length)
 }
 
-// add args to let users be able to select a specific location
-const options = yargs
-  .usage("Usage: -l <location>")
-  .option("l", {
-    alias: "location", 
-    describe: "A country name",
-    type: "string",
-    demandOption: false
-  })
-  .argv
-// ##############################################
-
-// message to display selected location
-const message = options.location ? `Selected Location: ${options.location.toUpperCase()}`: "No Location Selected";
-const msgBox = boxen( message, boxenOptions.locInfo )
-CFonts.say(' -- COVID-19 -- ', cfontsOptions.title);
-render(msgBox)
-// ##############################################
-
-// Display the results
-const displayResults = (data) => {
-  if(data) {
-    let lastUpdate = new Date()
-    if( options.location ) {
-      const { Last_Update, Confirmed = 0, Deaths = 0, Recovered = 0 } = data
-      const msgBoxDeaths = boxen( spaceFix(message, `Deaths: ${Deaths}`), boxenOptions.deaths )
-      const msgBoxConfirmed = boxen( spaceFix(message, `Confirmed: ${Confirmed}`), boxenOptions.confirmed )
-      const msgBoxRecovered = boxen( spaceFix(message, `Recovered: ${Recovered}`), boxenOptions.recovered )
-      render(msgBoxDeaths)
-      render(msgBoxConfirmed)
-      render(msgBoxRecovered)
-      
-      lastUpdate = Last_Update
-    } else {
-      const { Report_Date, Total_Confirmed = 0, Total_Recovered = 0 } = data
-      const msgBoxConfirmed = boxen( `Total Confirmed:\n${Total_Confirmed}`, boxenOptions.deaths )
-      const msgBoxRecovered = boxen( `Total Recovered:\n${Total_Recovered}`, boxenOptions.recovered )
-      render(msgBoxConfirmed)
-      render(msgBoxRecovered)
-      lastUpdate = Report_Date
-    }
-    
-    console.log('\n', chalk.bold.magenta('Last Update: ' + new Date(lastUpdate).toUTCString()));
-    CFonts.say('  -- end --  ', cfontsOptions.title);
-  } else {
-    const msgBoxNoDataFound = boxen( `No data found for: ${options.location}`)
-    render(msgBoxNoDataFound)
-  }
-}
-
-// ##############################################
-
-// Fetch data and display results
-async function fetchData() {
+const fetchData = async (options) => {
   const url = generateURL(options.location)
   await axios.get(url, { headers: { Accept: "application/json" } })
   .then(res => {
@@ -86,16 +33,121 @@ async function fetchData() {
         }
       }
       if (data.length === 0) {
-        console.log("no data found!")
+        render("no data found!")
       }
     } else {
       return res.data.features[0].attributes
     }
   })
   .then(res => {
-    displayResults(res)
+    displayResults(res, options)
   })
 }
 // ##############################################
 
-fetchData()
+
+// Display the results
+const displayResults = (data, options) => {
+
+  // message to display selected location
+  const message = options.location ? 
+  `Selected Location: ${options.location.toUpperCase()}`
+  : "No Location Selected"
+  const msgBox = boxen( message, boxenOptions.locInfo )
+  
+  CFonts.say(
+    '-- COVID-19 --', 
+    cfontsOptions.title
+  )
+  render(msgBox)
+  
+  if(data) {
+    let lastUpdate = new Date()
+    if( options.location ) {
+      const { 
+        Last_Update, 
+        Confirmed = 0, 
+        Deaths = 0, 
+        Recovered = 0 
+      } = data
+
+      const msgBoxDeaths = boxen( 
+        spaceFix(message, `Deaths: ${Deaths}`), 
+        boxenOptions.deaths
+      )
+
+      const msgBoxConfirmed = boxen( 
+        spaceFix(message, `Confirmed: ${Confirmed}`), 
+        boxenOptions.confirmed 
+      )
+
+      const msgBoxRecovered = boxen(
+        spaceFix(message, `Recovered: ${Recovered}`), 
+        boxenOptions.recovered 
+      )
+
+      render(msgBoxDeaths)
+      render(msgBoxConfirmed)
+      render(msgBoxRecovered)
+      lastUpdate = Last_Update
+    } else {
+      const { 
+        Report_Date, 
+        Total_Confirmed = 0, 
+        Total_Recovered = 0 
+      } = data
+
+      const msgBoxConfirmed = boxen(
+        `Total Confirmed:\n${Total_Confirmed}`,
+        boxenOptions.deaths,
+      )
+      
+      const msgBoxRecovered = boxen(
+        `Total Recovered:\n${Total_Recovered}`,
+        boxenOptions.recovered,
+      )
+
+      render(msgBoxConfirmed)
+      render(msgBoxRecovered)
+      lastUpdate = Report_Date
+    }
+    
+    console.log(
+      chalk
+        .bold
+        .magenta(
+          '\nLast Update: ' + new Date(lastUpdate).toUTCString()
+        )
+    );
+  } else {
+    const msgBoxNoDataFound = boxen(
+      `No data found for: ${options.location}`,
+      boxenOptions.locInfo,
+    )
+    render(msgBoxNoDataFound)
+  }
+
+  CFonts.say(
+    '  -- end --  ', 
+    cfontsOptions.title
+  );
+}
+
+
+// add args to let users be able to select a specific location
+const options = yargs
+  .usage(
+    "Usage: -l <location>"
+  )
+  .option(
+    "l", {
+    alias: "location", 
+    describe: "A country name",
+    type: "string",
+    demandOption: false
+  })
+  .argv
+
+// Call covid
+const covid = () => fetchData(options)
+covid()
